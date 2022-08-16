b981dbc76e3f46ef3d104ac46bf34e06e695b536
Martin Blahutka <blahec@Martins-MacBook-Pro.local>
2022-08-15 23:01:53 -0700

    intro update2

--- src/@lekoarts/gatsby-theme-cara/components/hero.tsx
+++ src/@lekoarts/gatsby-theme-cara/components/hero.tsx
@@ -31,25 +31,26 @@ const Hero = ({ offset, factor = 1 }: { offset: number; factor?: number }) => (
         {/* <Svg icon="tree" hiddenMobile width={8} color="icon_darkest" left="45%" top="10%" /> */}
       </UpDownWide>
       <Svg icon="trees" hiddenMobile width={32} color="icon_darker" left="5%" top="70%" />
-        <Svg icon="tree" width={16} color="icon_darker" left="35%" top="20%" />
-         <Svg icon="tree" width={24} color="icon_darkest" left="10%" top="40%" />
-          <Svg icon="tree" width={16} color="icon_darkest" left="60%" top="25%" />
-           <Svg icon="tree" width={32} color="icon_darker" left="-3%" top="100%" />
+      <Svg icon="tree" hiddenMobile width={16} color="icon_darker" left="30%" top="20%" />
+      <Svg icon="tree" hiddenMobile width={24} color="icon_darkest" left="-3%" top="40%" />
+      {/* <Svg icon="tree" hiddenMobile width={16} color="icon_darkest" left="60%" top="25%" /> */}
+      {/* <Svg icon="tree" hiddenMobile width={32} color="icon_darker" left="40%" top="110%" /> */}
       {/* <Svg icon="circle" width={6} color="icon_darkest" left="4%" top="20%" /> */}
       {/* <Svg icon="fire" hiddenMobile width={12}  stroke color="icon_darker" left="55%" top="65%" /> */}
       {/* <Svg icon="upDown" width={8} color="icon_darkest" left="95%" top="90%" /> */}
       <Svg icon="tent" hiddenMobile width={32} stroke color="Peru" left="25%" top="80%" />
       {/* <Svg icon="triangle" width={8} stroke color="icon_darker" left="25%" top="5%" /> */}
-        <Svg icon="mountain" width={48} color="icon_darker" left="70%" top="5%" /> 
+        <Svg icon="mountain" hiddenMobile width={48} color="icon_darker" left="70%" top="5%" /> 
       {/* <Svg icon="circle" width={64} color="icon_red" left="95%" top="5%" /> */}
       {/* <Svg icon="fire" width={64} color="icon_orange" left="85%" top="5%" /> */}
       {/* <Svg icon="box" hiddenMobile width={64} color="icon_purple" left="5%" top="90%" /> */}
       {/* <Svg icon="tree" hiddenMobile width={32} stroke color="icon_green" left="15%" top="90%" /> */}
       <Svg icon="trees"  hiddenMobile width={12} color="icon_darkest" left="20%" top="10%" />
-      <Svg icon="trees" width={20} hiddenMobile color="DarkSeaGreen" left="50%" top="10%" />
+      <Svg icon="trees" hiddenMobile width={20} hiddenMobile color="DarkSeaGreen" left="50%" top="15%" />
       {/* <Svg icon="tree" width={16} hiddenMobile stroke color="icon_darker" left="10%" top="50%" /> */}
-      <Svg icon="trees" width={64} color="icon_darker" left="90%" top="80%" />
-       <Svg icon="trees" width={56} color="icon_darkest" left="40%" top="50%" />
+      <Svg icon="trees"  hiddenMobile width={12} color="icon_darkest" left="20%" top="10%" />
+      <Svg icon="trees" hiddenMobile width={64} color="icon_darker" left="90%" top="80%" />
+       <Svg icon="trees"  width={56} color="icon_darkest" left="50%" top="50%" />
     </Divider>
     <Content sx={{ variant: `texts.bigger` }} speed={0.4} offset={offset} factor={factor}>
 
@@ -65,6 +66,7 @@ const Hero = ({ offset, factor = 1 }: { offset: number; factor?: number }) => (
           }}>
           Martin Blahutka
           </h1>
+          <hr width="100%" color="#2d3748" size="1px" align="right" />
         <Intro/>
       </Inner>
     </Content>
--- src/@lekoarts/gatsby-theme-cara/components/projects.tsx
+++ src/@lekoarts/gatsby-theme-cara/components/projects.tsx
@@ -35,7 +35,7 @@ const Projects = ({ offset, factor = 2 }: { offset: number; factor?: number }) =
       <UpDown>
         {/* <Svg icon="box" width={6} color="icon_brightest" left="85%" top="75%" /> */}
         <Svg icon="upDown" width={8} color="icon_teal" left="70%" top="20%" />
-        <Svg icon="triangle" width={8} stroke color="icon_orange" left="25%" top="5%" />
+        <Svg icon="triangle" width={8} stroke color="icon_pink" left="25%" top="5%" />
         <Svg icon="circle" hiddenMobile width={24} color="icon_brightest" left="17%" top="60%" />
         <Svg icon="circle" width={12} color="icon_pink" left="80%" top="60%" />
       </UpDown>
@@ -49,7 +49,7 @@ const Projects = ({ offset, factor = 2 }: { offset: number; factor?: number }) =
       </UpDownWide>
       <Svg icon="hexa" width={16} stroke color="icon_red" left="75%" top="30%" />
       <Svg icon="circle" hiddenMobile width={6} color="icon_brightest" left="4%" top="20%" />
-      <Svg icon="box" width={6} color="icon_orange" left="10%" top="10%" />
+      <Svg icon="box" width={6} color="icon_blue" left="10%" top="10%" />
       <Svg icon="box" width={12} color="icon_yellow" left="29%" top="26%" />
       <Svg icon="hexa" width={48} stroke color="icon_yellow" left="94%" top="70%" />
     </Divider>
